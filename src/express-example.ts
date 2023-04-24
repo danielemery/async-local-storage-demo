7fe4b043f8eb4092ba866907e3bae6c8e1747c27
Daniel Emery <daniel.emery@abusix.com>
2023-04-24 09:39:28 +0200

    Add a middleware that sometimes enriches the context

--- src/express-example.ts
+++ src/express-example.ts
@@ -7,16 +7,27 @@ const port = 3000;
 
 export type RequestContext = {
   cid: string;
+  randomEnrichment?: string;
 };
 
 const asyncLocalStorage = new AsyncLocalStorage<RequestContext>();
 
-app.use((req, res, next) => {
+// In the first middleware, initialise the async local storage
+app.use((req, _res, next) => {
   const cid = uuidv4();
   req.cid = cid;
   asyncLocalStorage.run({ cid }, () => {
     next();
   });
+});
+
+// In the second middleware, we can modify the existing async local storage
+app.use((_req, _res, next) => {
+  const context = asyncLocalStorage.getStore() ?? ({} as RequestContext);
+  if (Math.random() < 0.5) {
+    context.randomEnrichment = "Hello, world!";
+  }
+  next();
 });
 
 app.get("/", (req, res) => {
@@ -35,7 +46,8 @@ function exampleServiceFunction() {
   exampleLoggerFunction("Performing some business logic!");
 }
 
+// Now further down the stack, we can access the async local storage
 function exampleLoggerFunction(message: string) {
-  const { cid } = asyncLocalStorage.getStore() ?? {};
-  console.log(`${cid}: ${message}`);
+  const { cid, randomEnrichment } = asyncLocalStorage.getStore() ?? {};
+  console.log(`${cid}: ${message} <${randomEnrichment}>`);
 }
